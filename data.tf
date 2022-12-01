1baa7c482f133d7040773923848061f4ee0a4e5f
julanu <jula@Mariuss-MacBook-Pro.local>
2022-12-01 11:09:43 +0200

    add files that will bootstrap flux to the cluster

---
+++ data.tf
@@ -0,0 +1,24 @@
+# Flux
+data "flux_install" "main" {
+  target_path = var.target_path
+}
+
+data "flux_sync" "main" {
+  target_path = var.target_path
+  url         = "ssh://git@github.com/${var.github_owner}/${var.repository_name}.git"
+  branch      = var.branch
+}
+
+# Kubernetes
+data "kubectl_file_documents" "install" {
+  content = data.flux_install.main.content
+}
+
+data "kubectl_file_documents" "sync" {
+  content = data.flux_sync.main.content
+}
+
+# Github
+data "github_repository" "main" {
+  full_name = "${var.github_owner}/${var.repository_name}"
+}
